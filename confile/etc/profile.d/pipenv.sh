0405c4637296216f6c660f16274cbb11e41eec59
YJ-Work <yj1516268@outlook.com>
2020-12-09 15:10:43 +0800

    添加pipenv.sh

---
+++ confile/etc/profile.d/pipenv.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+: <<!
+Name: pipenv.sh
+Author: YJ
+Email: yj1516268@outlook.com
+Created Time: 2020-12-09 15:08:21
+
+Description: 配置pipenv变量
+!
+
+export PIPENV_PYPI_MIRROR="http://mirrors.aliyun.com/pypi/simple"
